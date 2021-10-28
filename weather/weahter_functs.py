0e5a090e2123d39251a52f6d6cec06cd6d3a5458
Samuel Kuld <61301218+SamuelKuld@users.noreply.github.com>
2021-10-27 23:57:53 -0700

    Update weahter_functs.py

--- weather/weahter_functs.py
+++ weather/weahter_functs.py
@@ -71,19 +71,22 @@ def get_type(type, data):
 
 
 def get_summary_as_string(data):
+    temperatures = get_type("temperature", data)
+    felt_temperatures = get_type("feels-like", data)
+
     return '\n'.join((f"Total Data Points: {len(data)}",
                       f"Timespan : {list(data.keys())[-1] - list(data.keys())[0]}s ",
                       f"  M : or {(list(data.keys())[-1] - list(data.keys())[0])/60}m ",
                       f"  H : or {(list(data.keys())[-1] - list(data.keys())[0])/3600}h.",
                       f"Average Distance of Time (Allows you to see how long it measures temp)= 1 / {get_average_difference_of_time(data)} Seconds",
                       f"  M : or 1 / {get_average_difference_of_time(data) / 60} Minutes",
-                      f"Standard Deviation of temperature : {get_standard_deviation_of_temp(get_type('temperature',data))}",
-                      f"Standard Deviation of felt temperature : {get_standard_deviation_of_feeling(get_type('feels-like',data))}",
-                      f"Average Temperature : {get_average_temperature(get_type('temperature',data))}",
-                      f"Average Felt Temperature : {get_average_feeling_temperature(get_type('feels-like',data))}",
-                      f"Minimum Temperature : {min(get_data_without_zero(get_type('temperature',data), 'temperature'))}",
-                      f"Max Temperature : {max([value['temperature'] for key, value in get_type('temperature',data).items()])}",
-                      f"Minimum Felt Temperature : {min(get_data_without_zero(get_type('feels-like',data), 'feels-like'))}",
+                      f"Standard Deviation of temperature : {get_standard_deviation_of_temp(temperatures)}",
+                      f"Standard Deviation of felt temperature : {get_standard_deviation_of_feeling(felt_temperatures)}",
+                      f"Average Temperature : {get_average_temperature(temperatures)}",
+                      f"Average Felt Temperature : {get_average_feeling_temperature(felt_temperatures)}",
+                      f"Minimum Temperature : {min(get_data_without_zero(temperatures, 'temperature'))}",
+                      f"Max Temperature : {max([value['temperature'] for key, value in temperatures.items()])}",
+                      f"Minimum Felt Temperature : {min(get_data_without_zero(felt_temperatures, 'feels-like'))}",
                       f"Max Felt Temperature : {max([value['feels-like'] for key, value in get_type('feels-like',data).items()])}"))  # Finish
 
 
